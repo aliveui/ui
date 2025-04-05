b6bd84043533e2f4842d8c4edb8d4fd0b4beeca5
prophecyengineer <hey.charlotte@icloud.com>
2025-04-05 20:12:41 +0100

    first commit

---
+++ apps/docs/app/global-error.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { Button } from '@repo/shadcn-ui/components/ui/button';
+import type NextError from 'next/error';
+import { fonts } from './lib/fonts';
+
+type GlobalErrorProperties = {
+  readonly error: NextError & { digest?: string };
+  readonly reset: () => void;
+};
+
+const GlobalError = ({ error, reset }: GlobalErrorProperties) => {
+  return (
+    <html lang="en" className={fonts}>
+      <body>
+        <h1>Oops, something went wrong</h1>
+        <Button onClick={() => reset()}>Try again</Button>
+      </body>
+    </html>
+  );
+};
+
+export default GlobalError;
